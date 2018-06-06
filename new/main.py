920ae1ee0afd81dd7ea12f68588a30a46d133c7e
Unknown <magnus.ja.gustafsson@gmail.com>
2018-06-06 09:35:56 +0200

    Added state

--- new/main.py
+++ new/main.py
@@ -1,7 +1,10 @@
 import pygame 
 import mouse
+import state
 
 screen = pygame.display.set_mode((1200, 800))
+pygame.font.init()
+font = pygame.font.SysFont("arial", 12)
 mytimer = pygame.time.Clock()
 
 if __name__ == "__main__":
