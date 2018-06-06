f9617b869974f25604843631d5aa0ab97db16b0f
Unknown <magnus.ja.gustafsson@gmail.com>
2018-06-06 09:30:29 +0200

    Introduced gui for button control

---
+++ new/state.py
@@ -0,0 +1,75 @@
+import pygame
+import sys
+import gui
+import mouse
+
+
+# tools
+TOOLS = {}
+SELECT = 0
+TOOLS[SELECT] = "Select"
+
+CREATEBOX = 1
+TOOLS[CREATEBOX] = "Create Box"
+
+CREATESPHERE = 2
+TOOLS[CREATESPHERE] = "Create Sphere"
+
+CREATECONNECTION = 3
+TOOLS[CREATECONNECTION] = "Create Connection"
+
+# State attributes
+tool = SELECT
+selected = []
+objects = []
+
+
+def run():
+    
+    pass
+
+
+if __name__ == "__main__":
+    screen = pygame.display.set_mode((800, 600))
+    mytimer = pygame.time.Clock()
+
+    def toolHandler(selectedTool):
+        global tool
+        tool = selectedTool
+        printTool()
+
+    def selectHandler():
+        toolHandler(SELECT)
+    selectButton = gui.button("Select", 10, 5, selectHandler)
+
+
+    def createBoxHandler():
+        toolHandler(CREATEBOX)
+    boxButton = gui.button("Box", 10, 35, createBoxHandler)
+
+    def createSphereHandler():
+        toolHandler(CREATESPHERE)
+    sphereButton = gui.button("Sphere", 10, 65, createSphereHandler)
+
+    def createConnectionHandler():
+        toolHandler(CREATECONNECTION)
+    connectionButton = gui.button("Connection", 10, 95, createConnectionHandler)
+
+    def printTool():
+        print TOOLS[tool]
+    printToolButton = gui.button("print tool", 300, 5, printTool)
+
+    while(True):
+        screen.fill(0xf6f6f6)
+        
+        mouse.run()
+        selectButton.run()
+        boxButton.run()
+        sphereButton.run()
+        connectionButton.run()   
+        printToolButton.run()
+        
+        run()
+
+        mytimer.tick(30)
+        pygame.display.flip() 
